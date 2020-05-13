1f64d1393b57a8e4738ff9e7bf9acc8e7778ad5b
santuan <smrcouto88@gmail.com>
2020-05-13 07:20:51 -0300

    fix gatsby

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import Headroom from "react-headroom"
-import ThemeToggler from "../components/ThemeToggler"
+import ThemeToggler from "../components/themeToggler"
 
 import "./header.css"
 import tw from "tailwind.macro"

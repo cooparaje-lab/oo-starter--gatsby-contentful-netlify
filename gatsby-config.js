05b15ddb7bf0dbeb50ead30e93c368f6b54b9aa3
santuan <smrcouto88@gmail.com>
2020-10-12 12:58:03 -0300

    yellouw

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,8 +101,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#fbb03c`,
+        theme_color: `#fbb03c`,
         display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },

96c20888eaa6537e4876101b56ccbe7107e587d1
santuan <smrcouto88@gmail.com>
2020-10-15 20:55:52 -0300

    lottie

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,90 @@
-import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Link } from "gatsby"
 import tw from "twin.macro"
 import styled from "@emotion/styled"
+import reactLogo from "../animations/working-together.json"
+import lottie from "lottie-web"
+import React, { useEffect } from "react"
 
 import Fade from "react-reveal/Fade"
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Inicio" />
-    <Home>
-      <MainTitle>
-        <Fade top cascade duration={1200}>
-          <h1>Recursos Gratuitos</h1>
-        </Fade>
-      </MainTitle>
-      <p className="text-xl">Hora de crear algo grandioso.</p>
-      <Link
-        className=""
-        tw="relative z-10 px-5 py-2 my-3 font-mono font-bold bg-white border-b-2 hover:border-indigo-500"
-        to="/blog/documento-legal-super-importante/"
-      >
-        Licencia de uso
-      </Link>
-      <div className="custom-shape-divider-bottom">
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-        >
-          <path
-            d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
-            opacity=".25"
-            className="shape-fill"
-          ></path>
-          <path
-            d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
-            opacity=".5"
-            className="shape-fill"
-          ></path>
-          <path
-            d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
-            className="shape-fill"
-          ></path>
-        </svg>
-      </div>
-    </Home>
-  </Layout>
-)
+const HomeComponent = () => {
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: document.querySelector("#react-logo"),
+      animationData: reactLogo,
+    })
+  }, [])
+
+  return (
+    <>
+      <Layout>
+        <SEO title="Inicio" />
+        <Home>
+          <div className="flex flex-col w-full py-6 md:py-8 ">
+            <section className="container relative z-50 flex flex-col flex-1 h-full mx-auto">
+              <div className="flex flex-col flex-1 h-full px-6">
+                <div className="flex items-center flex-1">
+                  <div className="grid items-center flex-1 grid-flow-row gap-8 py-8 md:grid-cols-4">
+                    <div className="col-span-2">
+                      <h1 className="font-serif text-4xl font-extrabold leading-tight md:text-6xl">
+                        Recursos Gratuitos
+                      </h1>
+
+                      <div className="pt-8 md:pt-12">
+                        <Link
+                          tw="relative z-10 px-5 mr-2 py-2 my-3 font-mono font-bold bg-white border-b-2 hover:border-indigo-500"
+                          to="/espacios/"
+                        >
+                          Espacios
+                        </Link>
+                        <Link
+                          tw="relative z-10 px-5 py-2 my-3 font-mono font-bold bg-white border-b-2 hover:border-indigo-500"
+                          to="/blog/documento-legal-super-importante/"
+                        >
+                          Licencia de uso
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="flex justify-end flex-1 col-span-2">
+                      <div id="react-logo" className="w-full h-full" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+
+          <div className="custom-shape-divider-bottom">
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1200 120"
+              preserveAspectRatio="none"
+            >
+              <path
+                d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+                opacity=".25"
+                className="shape-fill"
+              ></path>
+              <path
+                d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
+                opacity=".5"
+                className="shape-fill"
+              ></path>
+              <path
+                d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+                className="shape-fill"
+              ></path>
+            </svg>
+          </div>
+        </Home>
+      </Layout>
+    </>
+  )
+}
+
+export default HomeComponent
 
 const Home = styled.div`
   ${tw`relative flex flex-col items-center justify-center py-6 text-center bg-indigo-300`}
@@ -59,6 +95,9 @@ const Home = styled.div`
 
     p {
       ${tw`text-gray-300 `}
+    }
+    h1 {
+      ${tw`text-gray-100`}
     }
   }
 `
@@ -74,5 +113,3 @@ const MainTitle = styled.div`
     }
   }
 `
-
-export default IndexPage

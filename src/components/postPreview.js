a0ad438ec2864002554fb142cc95f46683182977
santuan <smrcouto88@gmail.com>
2020-10-27 13:18:00 -0300

    search ultra

--- src/components/postPreview.js
+++ src/components/postPreview.js
@@ -8,35 +8,38 @@ import styled from "@emotion/styled"
 import { Link } from "gatsby"
 import React from "react"
 import { Highlight } from "react-instantsearch-dom"
+import Fade from "react-reveal/Fade"
 import tw from "twin.macro"
+
 const PostPreview = ({ hit }) => {
   return (
-    <SearchItem>
+    <Fade cascade delay={150}>
+      <SearchItem>
         <Link to={hit.url}>
           <Highlight hit={hit} attribute="title" tagName="mark" />
         </Link>
-      <p>
-        <Highlight hit={hit} attribute="description" tagName="mark" />
-      </p>
-    </SearchItem>
+        <p>
+          <Highlight hit={hit} attribute="description" tagName="mark" />
+        </p>
+      </SearchItem>
+    </Fade>
   )
 }
 
 export default PostPreview
 
 const SearchItem = styled.div`
-  ${tw`bg-gray-100 p-6 pt-2 pb-4`}
+  ${tw`p-6 pt-2 pb-4 bg-gray-100`}
 
   body.dark & {
-      ${tw`text-indigo-100 bg-gray-800`}
+    ${tw`text-indigo-100 bg-gray-800`}
   }
-  
+
   a {
-      ${tw`font-mono text-3xl font-bold text-indigo-500`}
-     
+    ${tw`font-mono text-xl font-bold text-indigo-500`}
   }
 
   p {
-      ${tw`font-sans text-xl`}
+    ${tw`font-sans text-lg`}
   }
 `

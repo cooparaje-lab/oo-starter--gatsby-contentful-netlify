207e9c5a15c94e03ad53aa643569d96a51dab1c2
santuan <smrcouto88@gmail.com>
2020-10-28 02:29:15 -0300

    fix 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react"
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import lottie from "lottie-web"
@@ -10,8 +10,7 @@ import tw from "twin.macro"
 import styled from "@emotion/styled"
 
 const NotFoundPage = () => {
-
-    useEffect(() => {
+  useEffect(() => {
     lottie.loadAnimation({
       container: document.querySelector("#react-logo"),
       animationData: reactLogo,
@@ -20,35 +19,37 @@ const NotFoundPage = () => {
 
   return (
     <>
-
-  <Layout>
-    <SEO title="404" />
-    <Container>
-    <div className="absolute top-0 left-0 right-0 z-0 max-w-4xl mx-auto h-screen bg-react-logo  ">
-        <div id="react-logo" className="w-full h-full" />
-      </div>
-      <h1 className="mt-0 font-mono text-xl font-bold">Encontraste un 404</h1>
-
-      <Fade>
-      <SVGTaken className="svg-taken h-auto transition-opacity duration-500 transform relative z-50 max-w-md mx-auto" />
-      </Fade>
-    </Container>
-  </Layout>
+      <Layout>
+        <SEO title="404" />
+        <Container>
+          <div className="absolute top-0 left-0 right-0 z-0 h-screen max-w-4xl mx-auto bg-react-logo ">
+            <div id="react-logo" className="w-full h-full" />
+          </div>
+          <h1 className="mt-0 font-mono text-xl font-bold">
+            Encontraste un 404
+          </h1>
+          <Fade>
+            <SVGTaken className="relative z-50 h-auto max-w-md mx-auto transition-opacity duration-500 transform svg-taken" />
+          </Fade>
+        </Container>
+      </Layout>
     </>
   )
 }
 
 const Container = styled.div`
-  ${tw`flex h-screen  flex-col flex-wrap justify-center w-full bg-gray-100 pt-12 m-auto text-center`}
+  ${tw`flex flex-col flex-wrap justify-center w-full h-screen pt-12 m-auto text-center bg-gray-100`}
   body.dark & {
     ${tw`text-white bg-gray-900`}
 
     .bg-react-logo {
       opacity: 0;
+    }
 
-    } h1 {
-    opacity: 1;
-  }
+    h1 {
+      opacity: 1;
+    }
+
     .svg-taken {
       opacity: 1;
     }
@@ -59,14 +60,12 @@ const Container = styled.div`
   }
 
   .bg-react-logo {
-      opacity: 1;
-
-    }
+    opacity: 1;
+  }
 
   .svg-taken {
-      opacity: 0;
-    }
-
+    opacity: 0;
+  }
 `
 
 export default NotFoundPage
